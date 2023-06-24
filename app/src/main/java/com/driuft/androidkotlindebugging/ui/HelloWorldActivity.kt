6d065d95a55d763fb662e59a756c7b8e6eed1636
S Marbella <swmarbella@email.arizona.edu>
2023-06-24 15:03:48 -0700

    Added a user input getter to Hello World page
    Added a user input getter to Hello World page and a randomly generated default list of vehicle names if the user input is blank.

--- app/src/main/java/com/driuft/androidkotlindebugging/ui/HelloWorldActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/ui/HelloWorldActivity.kt
@@ -1,12 +1,39 @@
 package com.driuft.androidkotlindebugging.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.driuft.androidkotlindebugging.R
+
+private const val TAG = "HelloWorldActivity"
 
 class HelloWorldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello_world)
     }
+
+    fun nameTrain(view: View) {
+        val trainEditText = findViewById<EditText>(R.id.getUserInput)
+        var trainName = trainEditText.text.toString()
+        val randomTrains = listOf("AirTrain JFK", "Buffalo Metro Rail", "Polar Express",
+            "Dinosaur Train", "New York City Subway", "Port Authority Trans-Hudson",
+            "Staten Island Railway", "Acela Express", "Admiral", "Advance Commodore Vanderbilt",
+            "Advance Denver Zephyr", "Advance Empire State Express", "Advance Florida Special",
+            "Advance Flyer", "Baltimore-Washington Night Express", "Baltimore Day Express",
+            "Berkshire Hills Express", "Boardwalk Flyer", "Boston Express", "Buffalo Train",
+            "Albany Express", "Brazil and Mudlavia Express", "Butte Express", "Butte Special",
+            "Brainerd and International Falls Express", "Atlantic Express", "Jacksonville Express",
+            "Birmingham Special", "Atlanta Owl")
+        if (trainName == "") {
+            trainName = randomTrains.random()
+        }
+        val trainTextViewer = findViewById<TextView>(R.id.textView)
+        val phrase = "All aboard the $trainName!"
+        trainTextViewer.text = phrase
+    }
+
 }
